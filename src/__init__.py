b65f555e46afa1f4c3a8bcf0b6ac0773fcd0cba6
AmpiroMax <patrat2015@yandex.ru>
2023-02-14 12:41:42 +0300

    Dataset was added to __init__.py file

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from .pipeline import training_pipeline
 from .analysis import searcher_for_problem_examples
 from .model import RCNN
+from .dataset import CaptchaDataset
 
 __all__ = [
     "training_pipeline",
     "searcher_for_problem_examples",
-    "RCNN"
+    "RCNN",
+    "CaptchaDataset"
 ]
